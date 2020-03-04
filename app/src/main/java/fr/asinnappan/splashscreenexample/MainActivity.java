f3be3b8cd221f5b6b05d7c381549458c6240de19
Alain SINNAPPAN <Alain.Sinnappan@outlook.com>
2020-03-04 13:54:34 +0100

    Version test du projet final

--- app/src/main/java/fr/asinnappan/splashscreenexample/MainActivity.java
+++ app/src/main/java/fr/asinnappan/splashscreenexample/MainActivity.java
@@ -2,50 +2,58 @@ package fr.asinnappan.splashscreenexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.FirebaseApp;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    private static final int RC_SIGN_IN = 123;
+        Button button = findViewById(R.id.bouton_login);
 
-    Button button = (Button) findViewById(R.id.bouton_login);
+        FirebaseApp.getInstance();
 
-    button.setOnClickListener( new View.OnClickListener()
-    {
-        public void onClick (View v){
-        this.startSignInActivity(v);
-    }
-    });
-
-
-}
-
-         // 2 - Launch Sign-In Activity
-    private void startSignInActivity(){
-        startActivityForResult(
-                AuthUI.getInstance()
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+                startActivityForResult(AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setTheme(R.style.LoginTheme)
-                        .setAvailableProviders(
-                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
-                        .setIsSmartLockEnabled(false, true)
                         .setLogo(R.drawable.ic_logo_auth)
-                        .build(),
-                RC_SIGN_IN);
+                        .setIsSmartLockEnabled(false, true)
+                        .setAvailableProviders(providers)
+                        .build(), RC_SIGN_IN);
+            }
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                
+            }
+        }
+    }
 }
 
 
