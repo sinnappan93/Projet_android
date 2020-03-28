e55687ce79ed37e37e74be5f840e76fb67cb3060
Alain SINNAPPAN <Alain.Sinnappan@outlook.com>
2020-03-28 21:27:25 +0100

    Version test du projet final

--- app/src/main/java/fr/asinnappan/splashscreenexample/DisplayDevice.java
+++ app/src/main/java/fr/asinnappan/splashscreenexample/DisplayDevice.java
@@ -6,12 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-//import android.support.annotation.NonNull;
-//import android.support.annotation.Nullable;
-//import android.support.design.widget.FloatingActionButton;
-//import android.support.design.widget.Snackbar;
-//import android.support.v7.app.AppCompatActivity;
-
 public class DisplayDevice extends AppCompatActivity {
     private FloatingActionButton add;
 
